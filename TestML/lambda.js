4c778c1ec0cca32bb4530cfb5ed334dbfc5205a9
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-05-21 10:25:46 +0530

    TestML

--- TestML/lambda.js
+++ TestML/lambda.js
@@ -2,7 +2,7 @@ let AWS = require('aws-sdk');
 const ml = new AWS.MachineLearning();
 exports.handler = function (event, context, callback) {
 
-	ml.predict({
+	/*ml.predict({
 		MLModelId: 'ml-DYPsae14K89',
 		PredictEndpoint: 'https://realtime.machinelearning.us-east-1.amazonaws.com',
 		Record: {
@@ -13,7 +13,7 @@ exports.handler = function (event, context, callback) {
 	}, function (err, data) {
 		if (err) console.log(err, err.stack); // an error occurred
 		else console.log(data);           // successful response
-	});
+	});*/
 
 	callback(null, 'Successfully executed');
 }
